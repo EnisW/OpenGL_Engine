6a3e8df037b8637445d758c81e7cf3253da4fd4b
EnisW <enisakgun@eskisehir.edu.tr>
2024-11-01 22:43:35 +0300

    Init VBO, vertex shader and fragment shader definition and compiling

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow*, int, int);
-void process_input(GLFWwindow*);  
+void process_input(GLFWwindow*);
+void init_buffers(unsigned int&);
+
+const char* vertex_shader_source = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char* fragment_shader_source = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
 
 int main()
 {
@@ -28,7 +43,44 @@ int main()
     {   
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
-    }  
+    }
+
+    unsigned int VBO;
+    init_buffers(VBO);
+
+
+    // DEFINITION OF VERTEX SHADER AND COMPILING
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+
+    int vertex_success;
+    char vertex_info_log[512];
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &vertex_success);
+
+    if (!vertex_success)
+    {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, vertex_info_log);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << vertex_info_log << std::endl;
+    }
+
+    // DEFINITION OF FRAGMENT SHADER AND COMPILING
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+
+    int fragment_success;
+    char fragment_info_log[512];
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &fragment_success);
+
+    if (!fragment_success)
+    {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, fragment_info_log);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << fragment_info_log << std::endl;
+    }
+    
+    
+
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -43,7 +95,7 @@ int main()
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+    glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
 }
@@ -60,4 +112,19 @@ void process_input(GLFWwindow* window)
     {
         glfwSetWindowShouldClose(window, true);
     }
+}
+
+void init_buffers(unsigned int &VBO)
+{
+    glGenBuffers(1, &VBO); // CREATING 1 VBO OBJECT
+ 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // SETTING ACTIVE THE BUFFER OBJECT ()
+
+    float vertices[] = {  // CORDINATES FOR TRIANGLE 
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // LOADING VALUES TO VBO 
 }
